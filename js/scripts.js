22ffed7b03f9b0e74beaa6f5f93f8007a29fb23b
Matt <m_dittmann@yahoo.com>
2017-11-15 21:29:32 -0800

    add js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function getTarget(e) {
+  if (!e) {
+    e = window.event;
+  }
+  return e.target || e.srcElement;
+}
+
+// set up event listeners to call itemDone() on click event
+// get shopping list <ul>
+var el = document.getElementById('shoppingList');
+// if event listeners are supported
+if (el.addEventListener) {
