99c2e57e29619260e69b53ba72b3514559a245ec
verdantpath <m_dittmann@yahoo.com>
2017-11-20 16:31:04 -0800

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,27 @@ function getTarget(e) {
   return e.target || e.srcElement;
 }
 
+function itemDone(e) {
+  // remove item from the list
+  var target, elParent, elGrandparent
+  // get the item clicked link
+  target = getTarget(e);
+  // get its list item (parent element)
+  elParent = target.parentNode;
+  // get the list (ul parent element)
+  elGrandparent = target.parentNode.parentNode;
+  // remove the list item from the list
+  elGrandparent.removeChild(elParent);
+
+  // prevent the link from taking you away from the current page (default behavior)
+  if (e.preventDefault) {
+    e.preventDefault;
+  } else {
+    // for older versions of IE (lte IE8)
+    e.returnValue = false;
+  }
+}
+
 // set up event listeners to call itemDone() on click event
 // get shopping list <ul>
 var el = document.getElementById('shoppingList');
